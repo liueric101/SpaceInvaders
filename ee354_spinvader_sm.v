336e3cef8d62c6c66f6e109adcbcb2501f654aff
Eric Liu <liueric101@gmail.com>
2018-04-24 17:35:32 -0700

    shooting & moving

--- ee354_spinvader_sm.v
+++ ee354_spinvader_sm.v
@@ -11,13 +11,209 @@ module ee354_spinvaders_sm(Clk, reset, L, R, shoot, topY, midY, botY, position, 
 	 output reg[9:0] alien6X, alien7X,alien8X,alien9X,alien10X;
 	 output reg[9:0] alien11X, alien12X,alien13X,alien14X,alien15X;
 	 output reg[9:0] position, projectileX, projectileY;
-	 reg[9:0] topY, midY, botY;
+	 output reg[9:0] returnX, returnY;
+	 reg[9:0] topY, midY, botY, base;
 	 reg[5:0] score;
-
-	 always @(posedge Clk)
-	 begin
+	 reg[3:0] alienSel;
+	 reg direction;
+    always @(posedge Clk) //Movement of the ship
+		begin
+			if(reset)
+				position<= 400;
+			else if(L && ~R)
+				position<=position-5;
+			else if(R && ~L)
+				position<=position+5;	
+		end
+	always @(posedge Clk) //return fire tracking
+	begin
 		if(reset)
 		begin
+			alienSel <= 1;
+			returnX <= 0;
+			returnY <= 0;
+		end
+		else if( returnY >= 480 )
+		begin
+			case(alienSel)
+			4'd0: alienSel <= alienSel + 6;
+			4'd1: 
+			if(alien1X == 950) alienSel <=alienSel + 6;
+			else
+			begin
+				returnX <= alien1X;
+				returnY <= topY;
+			end
+			4'd2: 
+			if(alien2X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien2X;
+				returnY <= topY;
+			end
+			4'd3: 
+			if(alien3X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien3X;
+				returnY <= topY;
+			end
+			4'd4: 
+			if(alien4X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien4X;
+				returnY <= topY;
+			end
+			4'd5: 
+			if(alien5X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien5X;
+				returnY <= topY;
+			end
+			4'd6: 
+			if(alien6X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien6X;
+				returnY <= midY;
+			end
+			4'd7: 
+			if(alien7X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien7X;
+				returnY <= midY;
+			end
+			4'd8: 
+			if(alien8X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien8X;
+				returnY <= midY;
+			end
+			4'd9: 
+			if(alien9X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien9X;
+				returnY <= midY;
+			end
+			4'd10: 
+			if(alien10X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien10X;
+				returnY <= midY;
+			end
+			4'd11: 
+			if(alien11X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien11X;
+				returnY <= botY;
+			end
+			4'd12: 
+			if(alien12X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien12X;
+				returnY <= botY;
+			end
+			4'd13: 
+			if(alien13X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien13X;
+				returnY <= botY;
+			end
+			4'd14: 
+			if(alien14X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien14X;
+				returnY <= botY;
+			end
+			4'd15: 
+			if(alien15X == 950)alienSel <= alienSel + 6;
+			else
+			begin
+				returnX <= alien15X;
+				returnY <= botY;
+			end
+			endcase
+		end
+		else
+		begin
+			returnY <= returnY + 10;
+		end
+		if( returnY > 415 && returnY < 435 && returnX > position-10 && returnY < position+10)
+		begin
+			returnX <= 920;
+			returnY <= 0;
+			position <= 950;
+		end
+	end
+	 always @ (posedge Clk) //Projectile tracking
+	 begin
+		if(direction)
+		begin
+			if(base > 10)
+				direction <= 0;
+			else
+			begin
+				alien1X <= alien1X + 10; 		
+				alien2X <= alien2X + 10;
+				alien3X <= alien3X + 10;
+				alien4X <= alien4X + 10;
+				alien5X <= alien5X + 10;
+		
+				alien6X <= alien6X + 10;
+				alien7X <= alien7X + 10;
+				alien8X <= alien8X + 10;
+				alien9X <= alien9X + 10;
+				alien10X <= alien10X + 10;
+		
+				alien11X <= alien11X + 10;
+				alien12X <= alien12X + 10;
+				alien13X <= alien13X + 10;
+				alien14X <= alien14X + 10;
+				alien15X <= alien15X + 10;
+			end
+		end
+		else
+		begin
+			if(base == 0)
+				direction <=1;
+			else
+			begin
+				base <= base-1;
+				alien1X <= alien1X - 10; 		
+				alien2X <= alien2X - 10;
+				alien3X <= alien3X - 10;
+				alien4X <= alien4X - 10;
+				alien5X <= alien5X - 10;
+		
+				alien6X <= alien6X - 10;
+				alien7X <= alien7X - 10;
+				alien8X <= alien8X - 10;
+				alien9X <= alien9X - 10;
+				alien10X <= alien10X - 10;
+		
+				alien11X <= alien11X - 10;
+				alien12X <= alien12X - 10;
+				alien13X <= alien13X - 10;
+				alien14X <= alien14X - 10;
+				alien15X <= alien15X - 10;
+			end
+		end
+		if(reset)
+		begin
+			direction <=1;
+			base <=0;
+			projectileX <= 10'd900;
+			projectileY <= 0;
 			alien1X <= 10'd30; 		
 			alien2X <= 10'd130;
 			alien3X <= 10'd230;
@@ -40,32 +236,12 @@ module ee354_spinvaders_sm(Clk, reset, L, R, shoot, topY, midY, botY, position, 
 			midY <= 10'd90;
 			botY <= 10'd150;
 		end
-	 end
-	 
-    always @(posedge Clk) //Movement of the ship
-		begin
-			if(reset)
-				position<= 400;
-			else if(L && ~R)
-				position<=position-5;
-			else if(R && ~L)
-				position<=position+5;	
-		end
-		
-	 always @ (posedge Clk) //Projectile tracking
-	 begin
-		if(reset)
-		begin
-			projectileX <= 10'd900;
-			projectileY <= 0;
-		end
-		
 		 else if(projectileX == 10'd900 ) //No projectile 
 		 begin
 			if(shoot) //If shoot command is active. create the new projectile 
 			begin
-				projectileX = position;
-				projectileY = 400;
+				projectileX <= position;
+				projectileY <= 400;
 			end
 		 end
 		 else if(projectileY == 0) //If projectile hits top of screen, delete the projectile
